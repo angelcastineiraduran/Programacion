4db3a0433513fecd16ebd02c807e9e3b3835bb10
Angel Castineira Duran <acastineiraduran@danielcastelao.org>
2023-04-25 10:56:20 +0200

    bol33

---
+++ trimestre3/teoria/pruebaBorrar/src/pruebaborrar/Fillo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pruebaborrar;
+
+/**
+ *
+ * @author dam1
+ */
+public class Fillo extends Padre{
+
+    @Override
+    public String toString() {
+        return "Fillo{" + '}';
+    }
+    
+    
+    
+    
+}
